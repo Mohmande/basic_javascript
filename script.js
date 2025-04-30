89c99c8c3918f3372128441d9b320d1389e4f915
Mohmande <mohmande87@gmail.com>
2025-04-30 17:57:04 +0500

    adding first program of javascript

--- script.js
+++ script.js
@@ -6,10 +6,36 @@
 // console.log("*****")
 // console.log("*****")
 
-console.log ("my name is mustafa ali mohmand")
-console.log("*")
-console.log("**")
-console.log("***")
-console.log("****")
-console.log("*****")
-console.log("******")
+// console.log ("my name is mustafa ali mohmand")
+// console.log("*")
+// console.log("**")
+// console.log("***")
+// console.log("****")
+// console.log("*****")
+// console.log("******")
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JS Test</title>
+</head>
+<body>
+
+  <h2>Greet Me</h2>
+  <input type="text" id="nameInput" placeholder="Enter your name">
+  <button onclick="greetUser()">Greet</button>
+
+  <p id="greetingMessage"></p>
+
+  <script>
+    function greetUser() {
+      const name = document.getElementById("nameInput").value;
+      const message = "Hello, " + name + "! Welcome to JavaScript.";
+      document.getElementById("greetingMessage").innerText = message;
+    }
+  </script>
+
+</body>
+</html>
+
+
