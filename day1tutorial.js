729a7cf7519bfc038f0a9c3f26789358d0e4be88
Mohmande <mohmande87@gmail.com>
2025-05-01 22:03:36 +0500

    adding first file of js

---
+++ day1tutorial.js
@@ -0,0 +1,16 @@
+// let width =20
+// let height =10
+// let area =width*height
+// console.log(area)
+
+let mathsmarks =80
+let englishmarks =70
+let sciencemarks =90
+
+let totalnumber =300
+let obtainnumber =240
+
+ let percentage =((obtainnumber/totalnumber)*100)
+console.log(totalnumber)
+console.log(obtainnumber)
+console.log(percentage)
