d17636e257e92adbfbb722ab94cc9a919a5fced5
Mohmande <mohmande87@gmail.com>
2025-05-02 17:11:54 +0500

    ahmad bhai tasks

--- day1tutorial.js
+++ day1tutorial.js
@@ -3,14 +3,47 @@
 // let area =width*height
 // console.log(area)
 
-let mathsmarks =80
-let englishmarks =70
-let sciencemarks =90
+// let mathsmarks =80
+// let englishmarks =70
+// let sciencemarks =90
 
-let totalnumber =300
-let obtainnumber =240
+// let totalnumber =300
+// let obtainnumber =240
 
- let percentage =((obtainnumber/totalnumber)*100)
-console.log(totalnumber)
-console.log(obtainnumber)
-console.log(percentage)
+//  let percentage =((obtainnumber/totalnumber)*100)
+// console.log(totalnumber)
+// console.log(obtainnumber)
+// console.log(percentage)
+
+//finding speed of a object
+// let distance=120
+// let time=2
+// let speed=(distance/time)
+// console.log(speed)
+
+//conversion of celcius into fahrenheit
+
+// let celcius =30
+// let fahrenheit=(celcius*(9/5)+32)
+// console.log(fahrenheit)
+
+//find my average marks
+
+// let subject1=85
+// let subject2=90
+// let subject3=78
+// let subject4=92
+// let subject5=88
+// let averagemarks =(subject1+subject2+subject3+subject4+subject5/5)
+// console.log(averagemarks)
+
+//write a code of 10 power 9
+
+// let code=10
+// let power=(10*10*10*10*10*10*10*10*10)
+// console.log(power)
+
+// convert min into hours
+let min =120
+let hour =(min /60)
+console.log(hour)
