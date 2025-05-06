faf44b94a199aa7d1188636a67392d1e18ab1ebf
Mohmande <mohmande87@gmail.com>
2025-05-07 03:34:16 +0500

    adding age reporter to repository

---
+++ grade_reporter.js
@@ -0,0 +1,26 @@
+// grade reportor
+
+let score =78
+
+if(score<40)
+{
+    console.log("fail")
+}
+else if(score<=59)
+{
+    console.log("pass")
+
+}
+else if (score<=79)
+{
+    console.log("good")
+
+}
+else if (score<=100)
+{
+    console.log("excellect")
+
+}
+else{
+    console.log("invalid score")
+}
